0cf3c247b1cd294b7c7af2af3c83b050d618cfbe
Natalia Khodorova <icxodnik@gmail.com>
2025-03-17 05:53:07 +0100

    add parallel map-reduce

--- 2/compute_frequencies.py
+++ 2/compute_frequencies.py
@@ -1,10 +1,10 @@
 from collections import defaultdict
+from concurrent.futures import ProcessPoolExecutor
 import re
 import requests
 import matplotlib.pyplot as plt
 
-def map_function(text: str):
-    words = re.findall(r'[a-z]+', text.lower(), re.IGNORECASE)
+def map_function(words):
     return [(word, 1) for word in words]
 
 def shuffle_function(mapped_values):
@@ -19,10 +19,8 @@ def reduce_function(shuffled_values):
         reduced[key] = sum(values)
     return reduced
 
-# Виконання MapReduce
-def map_reduce(text):
-    # Крок 1: Мапінг
-    mapped_values = map_function(text)
+def map_reduce(words):
+    mapped_values = map_function(words)
 
     # Крок 2: Shuffle
     shuffled_values = shuffle_function(mapped_values)
@@ -31,6 +29,27 @@ def map_reduce(text):
     reduced_values = reduce_function(shuffled_values)
 
     return reduced_values
+
+def get_chunked_words(words, max_chunk_size):
+    words_chunks = [words[i:i+max_chunk_size] for i in range(0, len(words), max_chunk_size)]
+    return words_chunks
+
+def map_reduce_parallel(words, num_workers=4):
+    # compute complexity is not enough to justify parallelization comparing to the cost 
+    # of copying data between processes but it's a good exercise to understand how to use ProcessPoolExecutor
+    with ProcessPoolExecutor(max_workers=num_workers) as executor:
+        max_chunk_size = len(words)//num_workers
+        
+        words_chunks = get_chunked_words(words, max_chunk_size)
+        reduced_values = list(executor.map(map_reduce, words_chunks))
+        
+    # combine results from all workers
+    result = {}
+    for reduced_values in reduced_values:
+        print("len(reduced_values)", len(reduced_values))
+        for key, value in reduced_values.items():
+            result[key] = result.get(key, 0) + value
+    return result
 
 def visualize_top_words(result, num_words=10):
     plt.bar([x[0] for x in result[:num_words]], [x[1] for x in result[:num_words]])
@@ -45,7 +64,9 @@ if __name__ == '__main__':
     response = requests.get(url)
     text = response.text
     
-    result = map_reduce(text)
+    words = re.findall(r'[a-z]+', text.lower(), re.IGNORECASE)
+    
+    result = map_reduce_parallel(words)
     
     sorted_words = sorted(result.items(), key=lambda x: x[1], reverse=True)
     print(sorted_words[:10])
