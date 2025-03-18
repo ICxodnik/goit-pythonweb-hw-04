7aee121d7738bf76c1e3f8d086f04de7539d74d7
Natalia Khodorova <icxodnik@gmail.com>
2025-03-18 17:49:20 +0100

    made requested changes to use threading instead of multiprocessing

--- 2/compute_frequencies.py
+++ 2/compute_frequencies.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
-from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
 import re
+import time
 import requests
 import matplotlib.pyplot as plt
 
@@ -35,8 +36,24 @@ def get_chunked_words(words, max_chunk_size):
     return words_chunks
 
 def map_reduce_parallel(words, num_workers=4):
-    # compute complexity is not enough to justify parallelization comparing to the cost 
-    # of copying data between processes but it's a good exercise to understand how to use ProcessPoolExecutor
+    # now we're using ThreadPoolExecutor to comply with exercise requirements
+    # Probably it's not the best way to do it because of GIL and I expect that
+    # there would not be any performance gain comparing to sequential execution
+    # which we can check by calling this function with num_workers=1
+    with ThreadPoolExecutor(max_workers=num_workers) as executor:
+        max_chunk_size = len(words)//num_workers
+        
+        words_chunks = get_chunked_words(words, max_chunk_size)
+        reduced_values = list(executor.map(map_reduce, words_chunks))
+        
+    # combine results from all workers
+    result = {}
+    for reduced_values in reduced_values:
+        for key, value in reduced_values.items():
+            result[key] = result.get(key, 0) + value
+    return result
+
+def map_reduce_parallel_processes(words, num_workers=4):
     with ProcessPoolExecutor(max_workers=num_workers) as executor:
         max_chunk_size = len(words)//num_workers
         
@@ -46,7 +63,6 @@ def map_reduce_parallel(words, num_workers=4):
     # combine results from all workers
     result = {}
     for reduced_values in reduced_values:
-        print("len(reduced_values)", len(reduced_values))
         for key, value in reduced_values.items():
             result[key] = result.get(key, 0) + value
     return result
@@ -66,7 +82,28 @@ if __name__ == '__main__':
     
     words = re.findall(r'[a-z]+', text.lower(), re.IGNORECASE)
     
-    result = map_reduce_parallel(words)
+    
+    # warm up before actual measurements
+    map_reduce_parallel(words, 1)
+    map_reduce_parallel(words, 4)
+    map_reduce_parallel_processes(words, 1)
+    
+    time_start = time.time()
+    result = map_reduce_parallel_processes(words, 4)
+    time_end = time.time()
+    print(f"Time taken 4 processes: {time_end - time_start} seconds")
+    
+    
+    time_start = time.time()
+    result = map_reduce_parallel(words, 1)
+    time_end = time.time()
+    print(f"Time taken 1 thread: {time_end - time_start} seconds")
+    
+    time_start = time.time()
+    result = map_reduce_parallel(words, 4)
+    time_end = time.time()
+    print(f"Time taken 4 threads: {time_end - time_start} seconds")
+    
     
     sorted_words = sorted(result.items(), key=lambda x: x[1], reverse=True)
     print(sorted_words[:10])
